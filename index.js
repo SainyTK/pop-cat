7dc2cd48dcb3a8ac380df5049b39ae70e5b107c2
tanakorn0314 <tanakorn0412@gmail.com>
2021-08-15 11:26:15 +0700

    Thailand GOGO !!

---
+++ index.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    let browser;
+    try {
+        browser = await puppeteer.launch({
+            headless: false,
+            args: ["--disable-setuid-sandbox"],
+            'ignoreHTTPSErrors': true
+        });
+
+        const page = await browser.newPage();
+
+        await page.goto('https://popcat.click/');
+
+        await page.waitForSelector("body");
+
+        while (true) {
+            await page.click("body");
+            await page.waitForTimeout(0.05);
+        }
+
+    } catch (e) {
+        console.log("error", e);
+    }
+    await browser.close();
+})();
